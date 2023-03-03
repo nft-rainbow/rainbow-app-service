b8f603bf36114c4ffc62e1cefa5d5a227f844936
YangXianqi <xqyang0202@163.com>
2023-03-03 10:09:50 +0800

    fix the status

--- services/common.go
+++ services/common.go
@@ -215,7 +215,7 @@ func getTokenInfo(id int32, token string) (string, string, int32, error) {
 		return "", "", 0, err
 	}
 
-	for *resp.Status != models.STATUS_SUCCESS && *resp.Hash == "" {
+	for *resp.Status == models.STATUS_INIT {
 		resp, _, err = newClient().MintsApi.GetMintDetail(context.Background(), id).Authorization(token).Execute()
 		if err != nil {
 			return "", "", 0, err
