e200bab99a1892f0778a0a7107e10e5d0acf9cb3
dayong <wangdayong228@163.com>
2023-04-21 10:41:44 +0800

    change api botserver/invite_url to bot/invite_url

--- routers/router.go
+++ routers/router.go
@@ -36,8 +36,10 @@ func SetupRoutes(router *gin.Engine) {
 	apps := router.Group("/apps")
 	apps.GET("/swagger/xxx", gs.WrapHandler(swaggerFiles.Handler))
 
-	botServer := apps.Group("/botserver")
-	botServer.GET("/invite_url", botServerHandler.GetInviteUrl)
+	bot := apps.Group("/bot")
+	bot.GET("/invite_url", botServerHandler.GetInviteUrl)
+
+	botServer := bot.Group("/server")
 	botServer.Use(middlewares.JwtAuthMiddleware.MiddlewareFunc())
 	{
 		botServer.GET("/channels", botServerHandler.GetChannels)
