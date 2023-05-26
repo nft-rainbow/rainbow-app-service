94b8367b32cede6e3be03b014fcf3e2301dcd780
dyoung <wangdayong228@163.com>
2023-05-26 15:52:00 +0800

    Address whitelist v0 (#51)
    * support address white list
    
    * support address white list

--- services/activity.go
+++ services/activity.go
@@ -20,6 +20,7 @@ import (
 	"github.com/nft-rainbow/rainbow-app-service/utils"
 	randutils "github.com/nft-rainbow/rainbow-app-service/utils/rand"
 
+	. "github.com/ahmetalpbalkan/go-linq"
 	openapiclient "github.com/nft-rainbow/rainbow-sdk-go"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -344,6 +345,11 @@ func (a *ActivityService) GetMintCount(activityID, address string) (*int32, erro
 				return &count, nil
 			}
 		}
+	} else if config.IsAddressWhiteListOpened {
+		if !From(config.AddressWhitelist).Contains(address) {
+			count = 0
+			return &count, nil
+		}
 	}
 
 	mintedCount, err := models.GetMintSumByAddresses(activityID, address)
@@ -380,7 +386,7 @@ func (a *ActivityService) CheckMintable(config *models.Activity, req *MintReq) e
 		return errors.WithStack(err)
 	}
 
-	addrsOfPhone := []string{req.UserAddress}
+	var addrsOfPhone []string
 	if config.IsPhoneWhiteListOpened {
 		users, err := models.FindWalletUserByAddress(req.UserAddress)
 
@@ -405,6 +411,12 @@ func (a *ActivityService) CheckMintable(config *models.Activity, req *MintReq) e
 			return err
 		}
 		addrsOfPhone = append(addrsOfPhone, addrs...)
+	} else if config.IsAddressWhiteListOpened {
+		if !From(config.AddressWhitelist).Contains(req.UserAddress) {
+			return ERR_BUSINESS_NO_MINT_PERMISSIION
+		}
+	} else {
+		addrsOfPhone = append(addrsOfPhone, req.UserAddress)
 	}
 
 	if err := checkUserMintQuota(config.ActivityCode, addrsOfPhone, config.MaxMintCount); err != nil {
