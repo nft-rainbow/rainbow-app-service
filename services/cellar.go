9f4fbe912e7dd94b3670977988f1838380f8f760
dayong <wangdayong228@163.com>
2023-06-25 11:40:47 +0800

    use token get cellar user

--- services/cellar.go
+++ services/cellar.go
@@ -1,10 +1,15 @@
 package services
 
 import (
-	"errors"
+	"bytes"
+	"encoding/json"
+
+	"io/ioutil"
+	"net/http"
 
 	"github.com/nft-rainbow/rainbow-app-service/models"
 	"github.com/nft-rainbow/rainbow-app-service/models/enums"
+	"github.com/pkg/errors"
 	"gorm.io/gorm"
 )
 
@@ -16,13 +21,26 @@ func (a *Cellar) InsertUser(userReq AddWalletUserReq) error {
 		return errors.New("not cellar wallet")
 	}
 
-	_, err := models.FindWalletUser(userReq.Wallet, userReq.Address)
+	cu, err := a.getUserInfoByToken(userReq.Code)
+	if err != nil {
+		return err
+	}
+
+	if userReq.Address != "" && userReq.Address != cu.Data.Wallet {
+		return errors.New("address mismatch")
+	}
+
+	if userReq.Phone != "" && userReq.Phone != cu.Data.Phone {
+		return errors.New("phone mismatch")
+	}
+
+	_, err = models.FindWalletUser(enums.WALLET_CELLAR, cu.Data.Wallet)
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
 			user := &models.WalletUser{
-				Wallet:  userReq.Wallet,
-				Address: userReq.Address,
-				Phone:   userReq.Phone,
+				Wallet:  enums.WALLET_CELLAR,
+				Address: cu.Data.Wallet,
+				Phone:   cu.Data.Phone,
 			}
 			return models.GetDB().Create(user).Error
 		}
@@ -30,4 +48,39 @@ func (a *Cellar) InsertUser(userReq AddWalletUserReq) error {
 	}
 
 	return errors.New("user exists already")
+}
+
+type cellarUser struct {
+	Message string `json:"msg"`
+	Code    int    `json:"code"`
+	Data    struct {
+		Phone  string `json:"phone"`
+		Wallet string `json:"wallet"`
+	} `json:"data"`
+}
+
+func (a *Cellar) getUserInfoByToken(token string) (*cellarUser, error) {
+	url := "https://wallet.metacellar.art/web3/userAuth/getUserInfoByToken"
+	payload := map[string]string{"token": token}
+	payloadJ, _ := json.Marshal(payload)
+	resp, err := http.Post(url, "application/json", bytes.NewReader(payloadJ))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cu cellarUser
+	if err := json.Unmarshal(body, &cu); err != nil {
+		return nil, err
+	}
+
+	if cu.Code != http.StatusOK {
+		return nil, errors.Errorf("failed, code: %v, message: %v", cu.Code, cu.Message)
+	}
+
+	return &cu, nil
 }
